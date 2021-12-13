a94eb1d6a5feed37493b7632b7f0431fd0154b05
MetouiIsraa9 <73464669+MetouiIsraa9@users.noreply.github.com>
2021-12-13 11:07:28 +0100

    Update TestfinalApplicationTests.java

--- src/test/java/com/example/demo/TestfinalApplicationTests.java
+++ src/test/java/com/example/demo/TestfinalApplicationTests.java
@@ -1,17 +1,20 @@
-package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+
 @SpringBootTest
 class TestfinalApplicationTests {
 
 	@Test
-	public void shouldAnswerWithTrue()
+	public void nbPlaceInCar()
     {
-        assertTrue( true );
+		Place place = new Place ();
+		assertEquals("5",place.getHashcode()); 
     }
 
 }
